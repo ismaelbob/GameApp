906a6f0a786ceaeda9893d89746d49c3c0b83b99
bob <bobismaeljg@gmail.com>
2023-08-16 17:14:37 -0400

    Termine la vista para movil, falta para desktop

--- src/Principal.jsx
+++ src/Principal.jsx
@@ -1,10 +1,15 @@
-
+import './css/principal.css'
 
 function Principal () {
     return (
         <>
             <div className="pnl-principal">
                 <h4>DAME TRES</h4>
+                <div className='btn-play'></div>
+                <input type="number" name="txtnumber" id="txtnumber" />
+                <label htmlFor="txtnumber">Segundos</label>
+                <input type="button" value="INICIAR" />
+                <div className='menu'></div>
             </div>
         </>
     )
